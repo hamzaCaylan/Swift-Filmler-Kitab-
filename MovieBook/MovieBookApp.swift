a7e4546a25ac2aea88b04bfac01979378ebeb18d
hamzaçaylan <hamzacaylan021@gmail.come>
2023-06-09 13:36:37 +0300

    Initial Commit

---
+++ MovieBook/MovieBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookApp.swift
+//  MovieBook
+//
+//  Created by Hamza Caylan on 9.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
